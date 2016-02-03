2dd249d6e51b2d22743ebe9829dae76bc046d8a9
Amber <Amber@10-248-146-4.wireless.oregonstate.edu>
2016-02-03 13:25:50 -0800

    unit test 1 is done lol

--- dominion/unittest1.c
+++ dominion/unittest1.c
@@ -12,6 +12,7 @@ int main(int argc, char* argv[]){
 
     /*
      *create random numbers for arguments for buyCard (including randomly assigning values to gameState Struct fields: whoseTurn, numBuys, coins, and set phase to 0)
+     *put a random number on the command line
      *call buyCard
      *check if it returned 0
      */ 
@@ -19,11 +20,15 @@ int main(int argc, char* argv[]){
     srand(time(NULL));
 
     int ret;
-    int supplyPos = rand()%5;
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    int supplyPos = rand()%6;
+    int seed = rand()%100;
     struct gameState *state = malloc(sizeof(struct gameState));
+    initializeGame(2, k, seed, state);
+    
     state->whoseTurn = 0;
-    state->numBuys = rand()%2;
-    state->coins = rand()%15;
+    state->numBuys =rand()%2+1;
+    state->coins = rand()%10 + 1;
     state->phase = 0;
 
     ret = buyCard(supplyPos, state);
